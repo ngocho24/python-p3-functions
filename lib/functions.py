a79bb0f654fc44f941051e2d0ef370c12be302cb
Elijah Ngocho <elijahngocho24@gmail.com>
2023-11-22 10:11:14 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+    #greet (Elijah):
+    #print(f"Hello, {Elijah}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    #Hello, programmer!
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+#result=add(3,5)
+#print(result)
+
 
 def halve(number):
-    pass
+    return number/2
+#result=halve(8)
+#print(result)
